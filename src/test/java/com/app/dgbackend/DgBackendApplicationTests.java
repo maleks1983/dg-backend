5fef5f4c2a5434a9c6ec636b0a60c88f580debe7
MAleksandr <maleks1983@gmail.com>
2025-04-29 20:33:50 +0300

    first commit

---
+++ src/test/java/com/app/dgbackend/DgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
